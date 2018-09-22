d9e4f277e3d95e5990dfd410af072d246b5a4c93
BonaventuraAldino <aldysenda99@gmail.com>
2018-09-22 23:11:56 +0700

    PBO3-10117122-Latihan1-Biodata

---
+++ src/pbo3/pkg10117122/latihan1/biodata/PBO310117122Latihan1Biodata.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117122.latihan1.biodata;
+
+/**
+ * NAMA  : BONAVENTURA ALDINO SENDA SENI
+ * KELAS : PBO-3
+ * NIM   : 10117122
+ * 
+ */
+public class PBO310117122Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------------");
+        System.out.println("NAMA\t\t: Bonaventura Aldino Senda Seni");
+        System.out.println("ALAMAT\t\t: Jalan Dipatiukur no.98");
+        System.out.println("TEMPAT LAHIR\t: ENDE");
+        System.out.println("TANGGAL LAHIR\t: 28 September 1999");
+        System.out.println("----------------------");
+        System.out.println("HOBI\t\t: Sepak Bola");
+        System.out.println("MAKES\t\t: Bubur Ayam");
+        System.out.println("MIKES\t\t: Air Putih");
+    }
+}
